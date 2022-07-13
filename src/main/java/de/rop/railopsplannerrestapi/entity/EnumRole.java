3896bfc41fa1d6fdcd049e31c5bd63b8a7a88c51
stricker <de.rocketman@outlook.com>
2022-07-13 17:12:01 +0200

    refactor rest api

---
+++ src/main/java/de/rop/railopsplannerrestapi/entity/EnumRole.java
@@ -0,0 +1,7 @@
+package de.rop.railopsplannerrestapi.entity;
+
+public enum EnumRole {
+    ROLE_READER,
+    ROLE_PLANNER,
+    ROLE_ADMIN
+}
--- src/main/java/de/rop/railopsplannerrestapi/entity/User.java
+++ src/main/java/de/rop/railopsplannerrestapi/entity/User.java
@@ -5,18 +5,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 public class User extends Agent{
 
-    private String role;
-
     @Column(unique = true)
     private String username;
     @JsonIgnore
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private EnumRole role;
+
+
 }
